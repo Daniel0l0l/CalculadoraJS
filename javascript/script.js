92aff3fe95b00728cce600350b3bd421607c3244
Felipe <felipesilveira207@gmail.com>
2024-06-18 15:04:04 -0300

    Comite da calculadora

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+function soma() {
+   let num1 = Number(document.getElementById("num1").value)
+   let num2 = Number(document.getElementById("num2").value)
+
+   let resultado = num1 + num2
+
+   document.getElementById("Resultado").innerHTML = resultado.toFixed(2)
+}
+
+function subtracao () {
+   let num1 = Number(document.getElementById("num1").value)
+   let num2 = Number(document.getElementById("num2").value)
+
+   let resultado = num1 - num2
+
+   document.getElementById("Resultado").innerHTML = resultado.toFixed(2)
+}
+
+function multiplicacao () {
+   let num1 = Number(document.getElementById("num1").value)
+   let num2 = Number(document.getElementById("num2").value)
+
+   let resultado = num1 * num2
+
+   document.getElementById("Resultado").innerHTML = resultado.toFixed(2)
+}
+
+function divisao () {
+   let num1 = Number(document.getElementById("num1").value)
+   let num2 = Number(document.getElementById("num2").value)
+
+   let resultado = num1 / num2
+
+   document.getElementById("Resultado").innerHTML = resultado.toFixed(2)
+}
